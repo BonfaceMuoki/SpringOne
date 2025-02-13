b7e2b0b81bc75905504218c64f96bfcf60e1ca31
Bonface Muoki <bonface@yieldexchange.ca>
2025-02-13 11:51:46 +0300

    adding the formone bean and testing it from main

--- src/main/java/com/bonaet/App.java
+++ src/main/java/com/bonaet/App.java
@@ -1,5 +1,8 @@
 package com.bonaet;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,7 +12,9 @@ public class App
 
     public static void main( String[] args )
     {
-        FormOne fone = new FormOne();
+        //FormOne fone = new FormOne();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        FormOne fone =(FormOne) context.getBean("f_one");
         fone.printFormLabel();
         System.out.println( "Hello World!" );
     }
